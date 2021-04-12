b8b43ebe7c2e46b0182e7442066c0128de9a83c0
Kat Leight <leightkt@gmail.com>
2021-04-12 17:07:23 -0600

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import coverPhoto from './assets/cover.jpg'
+import Navigation from './Components/Navigation'
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <img className="cover-photo" src={coverPhoto} alt="a human queer human in a floral shirt and brown hat with fall mountains in the background" />
     </div>
   );
---
+++ src/Components/Navigation.js
@@ -0,0 +1,15 @@
+import './Navigation.css'
+
+function Navigation () {
+
+    return (
+        <nav>
+            <p>ABOUT</p>
+            <p>PROJECTS</p>
+            <p>RESUME</p>
+            <p>CONTACT</p>
+        </nav>
+    )
+}
+
+export default Navigation
