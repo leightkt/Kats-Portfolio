715fa4b692f9bf801815b67fc9db88019649d93c
Kat Leight <leightkt@gmail.com>
2021-04-14 12:23:18 -0600

    add link to resume

--- src/Containers/ResumeContainer.js
+++ src/Containers/ResumeContainer.js
@@ -1,8 +1,24 @@
 import './ResumeContainer.css'
+import { useState } from 'react'
+import { Document, Page, pdfjs } from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function ResumeContainer () {
+    const [numPages, setNumPages] = useState(null)
+    const [pageNumber, setPageNumber] = useState(1)
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages)
+    }
+
     return (
-        <h2>RESUME HERE</h2>
+        <a href="https://drive.google.com/file/d/131vSLqNGPMnzb1iJcrwe3sun4MbqK4yj/view?usp=sharing" target="_blank" rel="noreferrer">
+        <Document className="resume"
+            file='resume.pdf'
+            onLoadSuccess={onDocumentLoadSuccess}>
+            <Page className="page" pageNumber={ pageNumber } />
+        </Document>
+        </a>
     )
 }
 
