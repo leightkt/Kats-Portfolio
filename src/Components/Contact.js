d389f593879398d1ea34e3830d722752d710660c
Kat Leight <leightkt@gmail.com>
2021-04-15 11:35:26 -0600

    add medium icon to contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,15 +3,17 @@ import hangOut from '../assets/hang.jpg'
 import github from '../assets/github.png'
 import gmail from '../assets/gmail.png'
 import linkedIn from '../assets/linkedIn.png'
+import medium from '../assets/medium.png'
 
 
 function Contact () {
     return(
         <section className="contact-container">
             <h2>LET'S HANG OUT</h2>
-            <a href="mailto:leightkt@gmail.com"><img className="contact-icon" src={gmail} alt="gmail logo" /></a>
+            <a href="mailto:leightkt@gmail.com"><img className="contact-icon gmail" src={gmail} alt="gmail logo" /></a>
             <a href="https://github.com/leightkt"><img className="contact-icon github" src={github} alt="github logo" /></a>
             <a href="https://www.linkedin.com/in/kat-leight/"><img className="contact-icon linkedIn" src={linkedIn} alt="linkedIn logo" /></a>
+            <a href="https://kat-leight.medium.com/"><img className="contact-icon medium" src={medium} alt="medium icon"/></a>
             <img className="hang-out" src={ hangOut } alt="looking down on climber repelling from a mountain with a view 1000s of feet to the valley below" />
         </section>
     )
