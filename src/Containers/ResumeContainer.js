48d26567d9b278aae363148efd74ad9a7e364d89
Kat Leight <leightkt@gmail.com>
2021-04-15 15:17:39 -0600

    move photo and quote above resume

--- src/Containers/ResumeContainer.js
+++ src/Containers/ResumeContainer.js
@@ -15,6 +15,12 @@ function ResumeContainer () {
 
     return (
         <section className="resume-section">
+            <div className="summary">
+                <p>High energy personality with a curiousity for exploring new technology sparked from a spirited dedication to the mountains.</p>
+                <p>I thrive outside my comfort zone.</p>
+            </div>
+            <a className="show-on-small" href="https://drive.google.com/file/d/131vSLqNGPMnzb1iJcrwe3sun4MbqK4yj/view?usp=sharing">FULL RESUME</a>
+            <img className="mountain" src={mountain} alt="a climber standing on a mountain ridge" />
             <a className="resume-link" href="https://drive.google.com/file/d/131vSLqNGPMnzb1iJcrwe3sun4MbqK4yj/view?usp=sharing" target="_blank" rel="noreferrer">
             <Document className="resume hide-on-small"
                 file='resume.pdf'
@@ -22,12 +28,6 @@ function ResumeContainer () {
                 <Page className="page" pageNumber={ pageNumber } />
             </Document>
             </a>
-            <div className="summary">
-                <p>High energy personality with a curiousity for exploring new technology sparked from a spirited dedication to the mountains.</p>
-                <p>I thrive outside my comfort zone.</p>
-            </div>
-            <a className="show-on-small" href="https://drive.google.com/file/d/131vSLqNGPMnzb1iJcrwe3sun4MbqK4yj/view?usp=sharing">FULL RESUME</a>
-            <img className="mountain" src={mountain} alt="a climber standing on a mountain ridge" />
         </section>
     )
 }
