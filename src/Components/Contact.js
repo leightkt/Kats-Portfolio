d5889aefae95fd113725bfe8f16a99d3fd626fc9
Kat Leight <leightkt@gmail.com>
2021-04-15 13:58:10 -0600

    adjust icons on flex

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,10 +10,12 @@ function Contact () {
     return(
         <section className="contact-container">
             <h2>LET'S HANG OUT</h2>
-            <a href="mailto:leightkt@gmail.com"><img className="contact-icon gmail" src={gmail} alt="gmail logo" /></a>
-            <a href="https://github.com/leightkt"><img className="contact-icon github" src={github} alt="github logo" /></a>
-            <a href="https://www.linkedin.com/in/kat-leight/"><img className="contact-icon linkedIn" src={linkedIn} alt="linkedIn logo" /></a>
-            <a href="https://kat-leight.medium.com/"><img className="contact-icon medium" src={medium} alt="medium icon"/></a>
+            <div className="icon-div">
+                <a className="icon-link" href="mailto:leightkt@gmail.com"><img className="contact-icon gmail" src={gmail} alt="gmail logo" /></a>
+                <a className="icon-link" href="https://github.com/leightkt"><img className="contact-icon github" src={github} alt="github logo" /></a>
+                <a className="icon-link" href="https://www.linkedin.com/in/kat-leight/"><img className="contact-icon linkedIn" src={linkedIn} alt="linkedIn logo" /></a>
+                <a className="icon-link" href="https://kat-leight.medium.com/"><img className="contact-icon medium" src={medium} alt="medium icon"/></a>
+            </div>
             <img className="hang-out" src={ hangOut } alt="looking down on climber repelling from a mountain with a view 1000s of feet to the valley below" />
         </section>
     )
