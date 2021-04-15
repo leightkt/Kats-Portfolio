6e47f2797b8550e437558c86de82de55d9d3261f
Kat Leight <leightkt@gmail.com>
2021-04-15 09:39:48 -0600

    add video to single display project card

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -3,6 +3,7 @@ import PourTaste from '../assets/PourTaste.png'
 import Voice2Vibes from '../assets/Voice2Vibes.png'
 import BelayChain from '../assets/BelayChain.png'
 import Portfolio from '../assets/portfolio.png'
+import ReactPlayer from 'react-player'
 
 function Project ({ projectData, setProject, singleDisplay, setSingleDisplay }) {
 
@@ -16,10 +17,12 @@ function Project ({ projectData, setProject, singleDisplay, setSingleDisplay }) 
             <h3>{ projectData.name }</h3>
             {   singleDisplay 
                 ?   <>
-                        <img className="project-image-larger" src={ projectData.image } alt={ `${projectData.name} screenshot` }/>
+                        { projectData.demo 
+                            ? <ReactPlayer url={ projectData.demo } />
+                            : <img className="project-image-larger" src={ projectData.image } alt={ `${projectData.name} screenshot` }/>
+                        }
                         <p>Tech Used: { projectData.tech }</p>
                         <a className="project-link" href={ projectData.github }>Github Repo</a>
-                        { projectData.demo ? <a className="project-link" href={projectData.demo}>Demo</a> : null }
                         { projectData.link ? <a className="project-link" href={projectData.demo}>App Link</a> : null }
                     </>
                 :  <img className="project-image" src={ projectData.image } alt={ `${projectData.name} screenshot` }/>
