2feb53aa05cf6868bfdb7b14a30173f528474968
Kat Leight <leightkt@gmail.com>
2021-04-27 11:58:19 -0600

    add text and cat icon to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,11 @@
 import './Footer.css'
+import computerCat from '../assets/computer-cat.png'
 
 function Footer () {
     return (
         <footer>
-            <h1>Kat In The Stack</h1>
+            <h1><span className="cap">K</span>at In The Stack</h1>
+            <a href="https://github.com/leightkt"><img className="compu-cat" src={computerCat} alt="a cat in a computer" /></a>
         </footer>
     )
 }
