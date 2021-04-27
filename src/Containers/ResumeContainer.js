3d4784d3359b26ba5b0ebd3ac68757392be747df
Kat Leight <leightkt@gmail.com>
2021-04-27 11:21:54 -0600

    adjust width of project card

--- src/Containers/ResumeContainer.js
+++ src/Containers/ResumeContainer.js
@@ -15,6 +15,11 @@ function ResumeContainer () {
 
     return (
         <section className="resume-section">
+            <div className="summary">
+                <p>High energy personality with a curiousity for exploring new technology sparked from a spirited dedication to the mountains.</p>
+                <p>I thrive outside my comfort zone.</p>
+                <img className="mountain" src={mountain} alt="a climber standing on a mountain ridge" />
+            </div>
             <a className="resume-link" href="https://drive.google.com/file/d/131vSLqNGPMnzb1iJcrwe3sun4MbqK4yj/view?usp=sharing" target="_blank" rel="noreferrer">
                 <Document className="resume hide-on-small"
                     file='resume.pdf'
@@ -22,11 +27,6 @@ function ResumeContainer () {
                     <Page className="page" pageNumber={ pageNumber } />
                 </Document>
             </a>
-            <div className="summary">
-                <p>High energy personality with a curiousity for exploring new technology sparked from a spirited dedication to the mountains.</p>
-                <p>I thrive outside my comfort zone.</p>
-                <img className="mountain" src={mountain} alt="a climber standing on a mountain ridge" />
-            </div>
             <a className="show-on-small" href="https://drive.google.com/file/d/131vSLqNGPMnzb1iJcrwe3sun4MbqK4yj/view?usp=sharing">FULL RESUME</a>
             
         </section>
