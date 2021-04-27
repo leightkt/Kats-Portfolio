ef43bfe1a2c9243a5a0d4bad17afe26348a2a949
Kat Leight <leightkt@gmail.com>
2021-04-27 11:30:24 -0600

    adjust icons on mobile view

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 function Header () {
     return (
         <header>
-            <h1><span className="cap">K</span>at <span className="cap">I</span>n <span className="cap">T</span>he <span className="cap">S</span>tack</h1>
+            <h1><span className="cap">K</span>at <span className="cap">L</span>eight</h1>
         </header>
     )
 }
